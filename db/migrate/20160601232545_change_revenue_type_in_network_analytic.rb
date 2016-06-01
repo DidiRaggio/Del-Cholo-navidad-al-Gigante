dbcbf39422695182215843fdcbab0716baae0e4a
Didi Raggio <diego_raggio@hotmail.com>
2016-06-01 20:38:31 -0300

    change reveune datatype for decimal(65,5)

--- db/migrate/20160601232545_change_revenue_type_in_network_analytic.rb
+++ db/migrate/20160601232545_change_revenue_type_in_network_analytic.rb
@@ -1,5 +1,5 @@
 class ChangeRevenueTypeInNetworkAnalytic < ActiveRecord::Migration
   def change
-  	change_column :revenue, :decimal, :precision => 65, :scale => 4
+  	change_column :network_analytic, :revenue, :decimal, :precision => 65, :scale => 4
   end
 end
