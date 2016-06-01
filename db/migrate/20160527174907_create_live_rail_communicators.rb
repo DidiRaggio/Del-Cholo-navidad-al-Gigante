b2bfa90401f554c918253c2c8161209e0e31baa4
Didi Raggio <diego_raggio@hotmail.com>
2016-06-01 19:12:58 -0300

    removed migration files for the rake task methods

--- db/migrate/20160527174907_create_live_rail_communicators.rb
+++
@@ -1,8 +0,0 @@
-class CreateLiveRailCommunicators < ActiveRecord::Migration
-  def change
-    create_table :live_rail_communicators do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20160527175626_create_nadaserve_parsers.rb
+++
@@ -1,8 +0,0 @@
-class CreateNadaserveParsers < ActiveRecord::Migration
-  def change
-    create_table :nadaserve_parsers do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
